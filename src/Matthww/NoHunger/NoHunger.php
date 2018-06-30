ab535b00686b2e8fd4302c6b230075ade29a1fbe
Matthww <Matthww@users.noreply.github.com>
2018-06-30 19:41:23 +0200

    Forgot to remove these

--- src/Matthww/NoHunger/NoHunger.php
+++ src/Matthww/NoHunger/NoHunger.php
@@ -10,11 +10,6 @@ class NoHunger extends PluginBase implements Listener {
 
     public function onEnable() {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->getLogger()->notice("is enabled");
-    }
-
-    public function onDisable() {
-        $this->getLogger()->notice("is disabled!");
     }
 
     public function Hunger(PlayerExhaustEvent $exhaustEvent) {
